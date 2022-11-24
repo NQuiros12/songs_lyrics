797407b62472ba41763152d6d3c2dfbd167c6825
NQuiros12 <nicolasquiros12@gmail.com>
2022-11-24 11:24:43 -0300

    Agregue una celda de codigo en el draft que deberia permitirnos usar lyricsgenius (la misma que usa spotify) para traer la letra de la cancion

--- code/web_scrapper.py
+++ code/web_scrapper.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 URL= "https://www.azlyrics.com/lyrics/tylerthecreator/foreword.html"
 #URL ="https://www.azlyrics.com/lyrics/tylerthecreator/911mrlonely.html"
 #URL ="https://www.azlyrics.com/lyrics/radiohead/highdry.html"
+URL = "https://www.azlyrics.com/lyrics/eltonjohn/rocketmanithinkitsgoingtobealonglongtime.html"
 class_div = "Lyrics__Container-sc-1ynbvzw-6 YYrds"
 page = requests.get(URL)
 
